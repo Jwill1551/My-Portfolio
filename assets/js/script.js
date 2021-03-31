ac672302d1f97c3432684966ce5280d42372b987
Joshua Williams <47330900+Jwill1551@users.noreply.github.com>
2021-03-30 18:39:36 -0700

    New changes or files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
-const navSlide = () => {
-    const menu = document.querySelector('.menu');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+//Button types bellow
+$(document).ready(function(){
+    $("#day-btn, #large-btn, #small-btn").hide();
+})
 
-    menu.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+//night and day mode 
+$("#night-btn").click(function(){
+    $("#day-btn").show();
+    $("#night-btn").hide();
+    $("body").css("backgroundColor", "rgb(63, 63, 63)");
+    $(".template").css("boxShadow", "none");
+    $(".template").css("background-color", "rgb(46, 51, 51)");
+    $("nav").css("background-color", "rgb(46, 51, 51)");
+    $("nav").css("boxShadow", "none");
+    $("p").css("color", "white");
+    $(".details").css("background-color", "rgb(63, 63, 63)");
+    $(".pro-title").css("color", "white");
+    $(".head-title").css("background-color", "rgb(63, 63, 63)");
+    $("#introduction").css("background-color", "rgb(63, 63, 63)");
+    $("#nav-color-bar").css("background-color", "rgb(22, 158, 255)");
+})
 
-        //Animate Links 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
-            }
-        });
-        //Menu animation
-        menu.classList.toggle('toggle');
-    });
-}
-
-navSlide();
+$("#day-btn").click(function(){
+    $("#night-btn").show();
+    $("#day-btn").hide();
+    $("body").css("backgroundColor", "rgb(235, 235, 235)");
+    $(".template").css("boxShadow", "none");
+    $(".template").css("background-color", "white");
+    $("nav").css("boxShadow", "rgb(228, 223, 223)");
+    $("nav").css("boxShadow", "none");
+    $("p").css("color", "darkslategray");
+    $(".details").css("background-color", "rgb(235, 235, 235)");
+    $(".pro-title").css("color", "white");
+    $(".head-title").css("background-color", "rgb(235, 235, 235)");
+    $("#introduction").css("background-color", "rgb(235, 235, 235)");
+    $("#nav-color-bar").css("background-color", "salmon");
+})
